4ee4eeae2b198344e8d2f6dd29d52aa4d757ff27
Mitch <mbarron198@gmail.com>
2020-10-23 10:25:42 +1100

    fixed typo

--- 03.js
+++ 03.js
@@ -1,43 +1,43 @@
 // Flexible Compound Components
 // http://localhost:3000/isolated/exercise/03.js
 
-import React from 'react'
-import {Switch} from '../switch'
+import React from "react"
+import { Switch } from "../switch"
 
 const ToggleContext = React.createContext()
-ToggleContext.displayName = 'ToggleContext'
+ToggleContext.displayName = "ToggleContext"
 
-function Toggle({children}) {
+function Toggle({ children }) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
   return (
-    <ToggleContext.Provider value={{on, toggle}}>
+    <ToggleContext.Provider value={{ on, toggle }}>
       {children}
     </ToggleContext.Provider>
   )
 }
 
 function useToggle() {
-  const context = React.useToggle(ToggleContext)
+  const context = React.useContext(ToggleContext)
   if (!context) {
-    throw Error('useToggle must be used inside of <Toggle />')
+    throw Error("useToggle must be used inside of <Toggle />")
   }
   return context
 }
 
-function ToggleOn({children}) {
-  const {on} = useToggle(ToggleContext)
+function ToggleOn({ children }) {
+  const { on } = useToggle(ToggleContext)
   return on ? children : null
 }
 
-function ToggleOff({children}) {
-  const {on} = useToggle(ToggleContext)
+function ToggleOff({ children }) {
+  const { on } = useToggle(ToggleContext)
   return on ? null : children
 }
 
-function ToggleButton({...props}) {
-  const {on, toggle} = useToggle(ToggleContext)
+function ToggleButton({ ...props }) {
+  const { on, toggle } = useToggle(ToggleContext)
   return <Switch on={on} onClick={toggle} {...props} />
 }
 
